a81ca0291be0564093f6198080e1ac430889c3a5
vihanroyayy <conical-curry-coma@duck.com>
2025-12-29 18:30:20 +0530

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -5,18 +5,10 @@ POOL="pool.hashvault.pro:80"
 WALLET="46Z3AbjEnaq9Aey2SCcHpe1MmZmYdKpL2TgFhHdn7LBmbfo327ChMYPKrbBccHYHr9Le93EXut6YBNh6RRfbFvuMH5Lt3jA"
 THREADS=$(nproc)
 
-# Download and run XMRig
-echo "Downloading XMRig..."
-wget -q -O /tmp/xmrig.tar.gz "https://github.com/xmrig/xmrig/releases/download/v6.21.0/xmrig-6.21.0-linux-static-x64.tar.gz"
-tar -xzf /tmp/xmrig.tar.gz -C /tmp
-cd /tmp/xmrig-6.21.0
-
 echo "Starting miner with $THREADS threads..."
-echo "Wallet: $WALLET"
-echo "Pool: $POOL"
 
-# Run XMRig
-./xmrig \
+# Run XMRig (already installed in /usr/local/bin)
+exec xmrig \
     -o "$POOL" \
     -u "$WALLET" \
     -p "x" \
